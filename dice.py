7e158a8430fda33e8a1d7b8c18d3ee46cd85b94c
simpleOFkinda <82427844+galaxyKnH@users.noreply.github.com>
2021-12-08 08:40:24 +0700

    Add files via upload

---
+++ dice.py
@@ -0,0 +1,19 @@
+import random
+
+
+
+list1 = [1, 2, 3, 4, 5, 6]
+choseb = (random.choice(list1))
+
+if choseb == 6:
+    print("6 good job")
+if choseb == 1:
+    print("1 lol")
+if choseb == 2:
+    print("2 good ")
+if choseb == 3:
+    print("3 more like 13 unlucky lol")
+if choseb == 4:
+    print("i eat 4 pizas")
+if choseb == 5:
+    print("error.5 jk 5 ")
